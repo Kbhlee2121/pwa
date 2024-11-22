85aa83f2878de16210d4f0ea2fbe28d897edc4c6
Kristin Lee <kbhlee2121@gmail.com>
2024-11-21 17:23:35 -0800

    add buttons; add id

--- web-install/app.js
+++ web-install/app.js
@@ -12,15 +12,31 @@ if (navigator.serviceWorker) {
 }
 
 // Handle install buttons.
-const installPreset = (e) => {
+// Cross-origin requires 2 parameters.
+const installCrossOrigin = (e) => {
   // Make sure to update the visible code sample in index.html when updating
   // this.
-  let manifest_id = "https://amandabaker.github.io/pwa/web-install/index.html";
+  let manifest_id = "https://microsoftedge.github.io/Demos/pwa-pwastore/";
   let install_url = manifest_id;
   navigator.install(manifest_id, install_url);
 };
 
+// Same-origin with 2 params.
+const sameOriginTwoParams = (e) => {
+  let manifest_id = "https://kbhlee2121.github.io/pwa/web-install/";
+  let install_url = manifest_id;
+  navigator.install(manifest_id, install_url);
+}
+
+// Same-origin with 0 params.
+const sameOriginZeroParams = (e) => {
+  navigator.install();
+}
+
 // const installCustom = (e) => {}
 
-document.getElementById("installPreset").addEventListener("click", installPreset);
+document.getElementById("installCrossOrigin").addEventListener("click", installCrossOrigin);
+document.getElementById("sameOriginTwoParams").addEventListener("click", sameOriginTwoParams);
+document.getElementById("sameOriginZeroParams").addEventListener("click", sameOriginZeroParams);
+// document.getElementById("installPreset").addEventListener("click", installPreset);
 // document.getElementById("installCustom").addEventListener("click", installCustom);
