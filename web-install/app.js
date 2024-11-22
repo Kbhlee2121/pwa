68dbcba175da51741e759df392910fac2205b654
Kristin Lee <kbhlee2121@gmail.com>
2024-11-21 17:32:11 -0800

    update button id

--- web-install/app.js
+++ web-install/app.js
@@ -22,21 +22,21 @@ const installCrossOrigin = (e) => {
 };
 
 // Same-origin with 2 params.
-const sameOriginTwoParams = (e) => {
+const installSameOriginTwoParams = (e) => {
   let manifest_id = "https://kbhlee2121.github.io/pwa/web-install/";
   let install_url = manifest_id;
   navigator.install(manifest_id, install_url);
 }
 
 // Same-origin with 0 params.
-const sameOriginZeroParams = (e) => {
+const installSameOriginZeroParams = (e) => {
   navigator.install();
 }
 
 // const installCustom = (e) => {}
 
 document.getElementById("installCrossOrigin").addEventListener("click", installCrossOrigin);
-document.getElementById("sameOriginTwoParams").addEventListener("click", sameOriginTwoParams);
-document.getElementById("sameOriginZeroParams").addEventListener("click", sameOriginZeroParams);
+document.getElementById("installSameOriginTwoParams").addEventListener("click", installSameOriginTwoParams);
+document.getElementById("installSameOriginZeroParams").addEventListener("click", installSameOriginZeroParams);
 // document.getElementById("installPreset").addEventListener("click", installPreset);
 // document.getElementById("installCustom").addEventListener("click", installCustom);
