aef12e29820afa346cb8f2d965f8d29b4749e21b
Kristin Lee <kbhlee2121@gmail.com>
2024-11-22 17:35:00 -0800

    add changes

--- web-install/app.js
+++ web-install/app.js
@@ -11,32 +11,45 @@ if (navigator.serviceWorker) {
   registerServiceWorker();
 }
 
+const crossOriginBtn = document.getElementById("installCrossOrigin");
+const sameOriginTwoParamsBtn = document.getElementById("installSameOriginTwoParams");
+const sameOriginZeroParamsBtn = document.getElementById("installSameOriginZeroParams");
+
 // Handle install buttons.
 // Cross-origin requires 2 parameters.
 const installCrossOrigin = (e) => {
   // Make sure to update the visible code sample in index.html when updating
   // this.
-  let manifest_id = "https://diek.us/pwinter/";
-  let install_url = manifest_id;
+  // let manifest_id = "https://diek.us/pwinter/";
+  // let install_url = manifest_id;
+  let manifest_id = "https://diek.us/bubble/index.html";
+  let install_url = "https://diek.us/bubble/";
   navigator.install(manifest_id, install_url);
+  console.log("installCrossOrigin button clicked");
 };
 
 // Same-origin with 2 params.
 const installSameOriginTwoParams = (e) => {
-  let manifest_id_2 = "https://kbhlee2121.github.io/pwa/web-install/index.html";
+  let manifest_id_2 = "https://kbhlee2121.github.io/pwa/web-install/";
   let install_url_2 = manifest_id_2;
   navigator.install(manifest_id_2, install_url_2);
 }
 
 // Same-origin with 0 params.
 const installSameOriginZeroParams = (e) => {
-  navigator.install();
+  // navigator.install();
+  console.log("installSameOriginZeroParams button clicked");
 }
 
-// const installCustom = (e) => {}
+crossOriginBtn.addEventListener("click", installCrossOrigin);
+sameOriginTwoParamsBtn.addEventListener("click", installSameOriginTwoParams);
+sameOriginZeroParamsBtn.addEventListener("click", installSameOriginZeroParams);
 
-document.getElementById("installCrossOrigin").addEventListener("click", installCrossOrigin);
-document.getElementById("installSameOriginTwoParams").addEventListener("click", installSameOriginTwoParams);
-document.getElementById("installSameOriginZeroParams").addEventListener("click", installSameOriginZeroParams);
+
+
+// const installCustom = (e) => {}
+// document.getElementById("installCrossOrigin").addEventListener("click", installCrossOrigin);
+// document.getElementById("installSameOriginTwoParams").addEventListener("click", installSameOriginTwoParams);
+// document.getElementById("installSameOriginZeroParams").addEventListener("click", installSameOriginZeroParams);
 // document.getElementById("installPreset").addEventListener("click", installPreset);
 // document.getElementById("installCustom").addEventListener("click", installCustom);
