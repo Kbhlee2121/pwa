d4469e8689a9562b54aa8beaf25a13a4ded470c6
Kristin Lee <kbhlee2121@gmail.com>
2024-11-22 17:42:58 -0800

    add different values

--- web-install/app.js
+++ web-install/app.js
@@ -20,19 +20,16 @@ const sameOriginZeroParamsBtn = document.getElementById("installSameOriginZeroPa
 const installCrossOrigin = (e) => {
   // Make sure to update the visible code sample in index.html when updating
   // this.
-  // let manifest_id = "https://diek.us/pwinter/";
-  // let install_url = manifest_id;
   let manifest_id = "https://diek.us/bubble/index.html";
   let install_url = "https://diek.us/bubble/";
   navigator.install(manifest_id, install_url);
-  console.log("installCrossOrigin button clicked");
 };
 
 // Same-origin with 2 params.
 const installSameOriginTwoParams = (e) => {
-  let manifest_id_2 = "https://kbhlee2121.github.io/pwa/web-install/";
-  let install_url_2 = manifest_id_2;
-  navigator.install(manifest_id_2, install_url_2);
+  let manifest_id = "https://kbhlee2121.github.io/pwa/web-install/";
+  let install_url = "https://kbhlee2121.github.io/pwa/web-install/index.html";
+  navigator.install(manifest_id, install_url);
 }
 
 // Same-origin with 0 params.
