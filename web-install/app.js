5b3c3cc7daf4bbaac444413c219ee26ebfffecc2
Kristin Lee <kristinlee@microsoft.com>
2025-07-02 13:13:02 -0700

    Update handler; reorganize

--- web-install/app.js
+++ web-install/app.js
@@ -11,125 +11,194 @@ if (navigator.serviceWorker) {
   registerServiceWorker();
 }
 
-const sameOriginTwoParamsBtn = document.getElementById("installSameOriginTwoParamsSwitched");
+// Feature detection.
+if (!navigator.install) {
+  console.error("navigator.install is not available.");
+}
+
+// Intended Use Cases
 const sameOriginZeroParamsBtn = document.getElementById("installSameOriginZeroParams");
-const sameOriginTwoParamSwitchedBtn = document.getElementById("installSameOriginTwoParamsSwitched");
-const crossOriginParamsSwitchedBtn = document.getElementById("installCrossOriginParamsSwitched");
+const crossOriginOneParamBtn = document.getElementById("installCrossOriginOneParam");
+const crossOriginParamsTwoParamsBtn = document.getElementById("installCrossOriginTwoParams");
+const installCrossOriginScreenshotsBtn = document.getElementById("installCrossOriginScreenshots");
+// DataError Cases
 const installOneParamNoDefinedIdBtn = document.getElementById("installOneParamNoDefinedId");
 const installTwoParamsMismatchedDefinedIdBtn = document.getElementById("installTwoParamsMismatchedDefinedId");
 const installTwoParamsMismatchedNoDefinedIdBtn = document.getElementById("installTwoParamsMismatchedNoDefinedId");
-const sameOriginOneParamBtn = document.getElementById("installSameOriginOneParam");
-const crossOriginOneParamBtn = document.getElementById("installCrossOriginOneParam");
+// TypeError Cases
 const installOneParamUndefinedBtn = document.getElementById("installOneParamUndefined");
 const installOneParamInvalidUrlBtn = document.getElementById("installOneParamInvalidUrl");
 const installTwoParamsUndefinedUrlBtn = document.getElementById("installTwoParamsUndefinedUrl");
 const installTwoParamsUndefinedManifestIdBtn = document.getElementById("installTwoParamsUndefinedManifestId");
-const installCrossOriginScreenshotsBtn = document.getElementById("installCrossOriginScreenshots");
-// Handle install buttons.
-const installCrossOriginOneParam = (e) => {
-  let install_url = "https://mustjab.github.io/";
-  navigator.install(install_url);
-}
+// Edge Cases - Installing self with params
+const sameOriginOneParamBtn = document.getElementById("installSameOriginOneParam");
+const sameOriginTwoParamsBtn = document.getElementById("installSameOriginTwoParams");
 
-// 1 param install url with no id defined in the web app manifest.
-const installOneParamNoDefinedId = (e) => {
-  // Amanda's web install sample app that doesn't have an ID defined in the manifest.
-  let install_url = "https://amandabaker.github.io/pwa/web-install/index.html";
-  navigator.install(install_url);
-}
-
-// 2 param install url WITH an id defined in the manifest, but mismatched input param
-const installTwoParamsMismatchedDefinedId = (e) => {
-  let install_url = "https://mustjab.github.io/";
-  let manifest_id = "mismatched-manifest-id";
-  navigator.install(install_url, manifest_id);
-}
-
-const installTwoParamsMismatchedNoDefinedId = (e) => {
-  let install_url = "https://amandabaker.github.io/pwa/web-install/index.html";
-  let manifest = "mismatched-manifest-id";
-  navigator.install(install_url, manifest);
-}
-
-// Same-origin with 2 params.
-const installSameOriginTwoParams = (e) => {
-  let manifest_id = "https://kbhlee2121.github.io/WebInstallSample";
-  let install_url = "https://kbhlee2121.github.io/pwa/web-install/index.html";
-  navigator.install(manifest_id, install_url);
-}
-
-// Cross-origin with 2 params switched.
-const installCrossOriginParamsSwitched = (e) => {
-  // Make sure to update the visible code sample in index.html when updating
-  // this.
-  let manifest_id = "https://diek.us/bubble/index.html";
-  let install_url = "https://diek.us/bubble/";
-  navigator.install(install_url, manifest_id);
-};
-
-const installCrossOriginScreenshots = (e) => {
-  let install_url = "https://squoosh.app/";
-  let manifest_id = "https://squoosh.app/?utm_medium=PWA&utm_source=launcher";
-  navigator.install(install_url, manifest_id);
-}
-
-// Same-origin with 2 params switched.
-const installSameOriginTwoParamsSwitched = (e) => {
-  let install_url = "https://kbhlee2121.github.io/pwa/web-install/index.html";
-  let manifest_id = "https://kbhlee2121.github.io/WebInstallSample";
-  navigator.install(install_url, manifest_id);
-}
+// ------- HANDLER FUNCTIONS --------
+// Intended Use Cases
 
 // Same-origin with 0 params.
-const installSameOriginZeroParams = (e) => {
-  // Uncomment when 0 param is supported end to end.
-  navigator.install();
-  console.log("installSameOriginZeroParams button clicked.");
-}
+sameOriginZeroParamsBtn.addEventListener("click", async() => {
+  try {
+    await navigator.install().then((result) => {
+      console.log(result);
+    });
+  } catch (err) {
+    console.error(err);
+  } 
+});
 
-const installSameOriginOneParam = (e) => {
-  let install_url = "https://kbhlee2121.github.io/pwa/web-install/index.html";
-  navigator.install(install_url);
-}
+// Cross-origin with 1 param.
+crossOriginOneParamBtn.addEventListener("click", async() => {
+  try {
+    let install_url = "https://mustjab.github.io/";
+    await navigator.install(install_url).then((result) => {
+      console.log(result);
+    });
+  } catch (err) {
+    console.error(err);
+  } 
+});
 
-const installOneParamUndefined = (e) => {
-  let install_url;
-  navigator.install(install_url);
-}
+// Cross-origin with 2 params.
+crossOriginParamsTwoParamsBtn.addEventListener("click", async() => {
+  try {
+    // Make sure to update the visible code sample in index.html when updating
+    // this.
+    let manifest_id = "https://diek.us/bubble/index.html";
+    let install_url = "https://diek.us/bubble/";
+    await navigator.install(install_url, manifest_id).then((result) => {
+      console.log(result);
+    });
+  } catch (err) {
+    console.error(err);
+  } 
+});
 
-const installOneParamInvalidUrl = (e) => {
-  navigator.install("badurl");
-}
+// A PWA that shows the detailed install dialog. For internal testing.
+// As of 2025-07-02: detailed install dialog is not yet supported.
+installCrossOriginScreenshotsBtn.addEventListener("click", async() => {
+  try {
+    let install_url = "https://squoosh.app/";
+    let manifest_id = "https://squoosh.app/?utm_medium=PWA&utm_source=launcher";
+    await navigator.install(install_url, manifest_id).then((result) => {
+      console.log(result);
+    });
+  } catch (err) {
+    console.error(err);
+  } 
+});
 
-const installTwoParamsUndefinedUrl = (e) => {
-  let install_url;
-  let manifest_id = "https://diek.us/bubble/index.html";
-  navigator.install(install_url, manifest_id);
-}
+// DataError Cases
 
-const installTwoParamsUndefinedManifestId = (e) => {
-  let install_url = "https://kbhlee2121.github.io/pwa/web-install/index.html";
-  let manifest_id;
-  navigator.install(install_url, manifest_id);
-}
+// 1 param install url with no id defined in the web app manifest.
+installOneParamNoDefinedIdBtn.addEventListener("click", async() => {
+  try {
+    // Amanda's web install sample app that doesn't have an ID defined in the manifest.
+    let install_url = "https://amandabaker.github.io/pwa/web-install/index.html";
+    await navigator.install(install_url).then((result) => {
+      console.log(result);
+    });
+  } catch (err) {
+    console.error(err);
+  } 
+});
 
-sameOriginTwoParamsBtn.addEventListener("click", installSameOriginTwoParams);
-sameOriginZeroParamsBtn.addEventListener("click", installSameOriginZeroParams);
-sameOriginTwoParamSwitchedBtn.addEventListener("click", installSameOriginTwoParamsSwitched);
-crossOriginParamsSwitchedBtn.addEventListener("click", installCrossOriginParamsSwitched);
-installOneParamNoDefinedIdBtn.addEventListener("click", installOneParamNoDefinedId);
-installTwoParamsMismatchedDefinedIdBtn.addEventListener("click", installTwoParamsMismatchedDefinedId);
-installTwoParamsMismatchedNoDefinedIdBtn.addEventListener("click", installTwoParamsMismatchedNoDefinedId);
-sameOriginOneParamBtn.addEventListener("click", installSameOriginOneParam);
-crossOriginOneParamBtn.addEventListener("click", installCrossOriginOneParam);
-installOneParamUndefinedBtn.addEventListener("click", installOneParamUndefined);
-installOneParamInvalidUrlBtn.addEventListener("click", installOneParamInvalidUrl);
-installTwoParamsUndefinedUrlBtn.addEventListener("click", installTwoParamsUndefinedUrl);
-installTwoParamsUndefinedManifestIdBtn.addEventListener("click", installTwoParamsUndefinedManifestId);
-installCrossOriginScreenshotsBtn.addEventListener("click", installCrossOriginScreenshots);
-// const installCustom = (e) => {}
-// document.getElementById("installCrossOrigin").addEventListener("click", installCrossOrigin);
-// document.getElementById("installSameOriginTwoParams").addEventListener("click", installSameOriginTwoParams);
-// document.getElementById("installSameOriginZeroParams").addEventListener("click", installSameOriginZeroParams);
-// document.getElementById("installPreset").addEventListener("click", installPreset);
-// document.getElementById("installCustom").addEventListener("click", installCustom);
+// 2 param install url WITH an id defined in the manifest, but mismatched input param.
+installTwoParamsMismatchedDefinedIdBtn.addEventListener("click", async() => {
+  try {
+    let install_url = "https://mustjab.github.io/";
+    let manifest_id = "mismatched-manifest-id";
+    await navigator.install(install_url, manifest_id).then((result) => {
+      console.log(result);
+    });
+  } catch (err) {
+    console.error(err);
+  } 
+});
+// 2 param install url WITHOUT an id defined in the manifest, mismatched input param.
+installTwoParamsMismatchedNoDefinedIdBtn.addEventListener("click", async() => {
+  try {
+    let install_url = "https://amandabaker.github.io/pwa/web-install/index.html";
+    let manifest = "mismatched-manifest-id";
+    await navigator.install(install_url, manifest).then((result) => {
+      console.log(result);
+    });
+  } catch (err) {
+    console.error(err);
+  } 
+});
+
+// V8 TypeError Cases
+
+installOneParamUndefinedBtn.addEventListener("click", async() => {
+  try {
+    let install_url;
+    await navigator.install(install_url).then((result) => {
+      console.log(result);
+    });
+  } catch (err) {
+    console.error(err);
+  } 
+});
+
+installOneParamInvalidUrlBtn.addEventListener("click", async() => {
+  try {
+    await navigator.install("badurl").then((result) => {
+      console.log(result);
+    });
+  } catch (err) {
+    console.error(err);
+  } 
+});
+
+installTwoParamsUndefinedUrlBtn.addEventListener("click", async() => {
+  try {
+    let install_url;
+    let manifest_id = "https://diek.us/bubble/index.html";
+    await navigator.install(install_url, manifest_id).then((result) => {
+      console.log(result);
+    });
+  } catch (err) {
+    console.error(err);
+  } 
+});
+
+installTwoParamsUndefinedManifestIdBtn.addEventListener("click", async() => {
+  try {
+    let install_url = "https://kbhlee2121.github.io/pwa/web-install/index.html";
+    let manifest_id;
+    await navigator.install(install_url, manifest_id).then((result) => {
+      console.log(result);
+    });
+  } catch (err) {
+    console.error(err);
+  } 
+});
+
+// Edge Cases - Installing self with params
+
+// Same-origin with 1 param.
+sameOriginOneParamBtn.addEventListener("click", async() => {
+  try {
+    let install_url = "https://kbhlee2121.github.io/pwa/web-install/index.html";
+    await navigator.install(install_url).then((result) => {
+      console.log(result);
+    });
+  } catch (err) {
+    console.error(err);
+  } 
+});
+
+// Same-origin with 2 params.
+sameOriginTwoParamsBtn.addEventListener("click", async() => {
+  try {
+    let install_url = "https://kbhlee2121.github.io/pwa/web-install/index.html";
+    let manifest_id = "https://kbhlee2121.github.io/WebInstallSample";
+    await navigator.install(install_url, manifest_id).then((result) => {
+      console.log(result);
+    });
+  } catch (err) {
+    console.error(err);
+  } 
+});
